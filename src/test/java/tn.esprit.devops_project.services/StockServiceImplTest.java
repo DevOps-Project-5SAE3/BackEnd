dfcc8212a68930a3052c54dd3bc8d473ab54d99f
FatmaMbarek <fatma.mbarek@esprit.tn>
2023-11-06 23:47:57 +0100

    add junit test

---
+++ src/test/java/tn.esprit.devops_project.services/StockServiceImplTest.java
@@ -0,0 +1,28 @@
+package tn.esprit.devops_project.services;
+
+import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import tn.esprit.devops_project.entities.Stock;
+import tn.esprit.devops_project.repositories.StockRepository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+public class StockServiceImplTest {
+    private StockServiceImpl stockService;
+    private StockRepository stockRepository;
+    private Stock stock;
+
+    @Test
+    public void testAddStock() {
+        stockRepository = Mockito.mock(StockRepository.class);
+        stockService = new StockServiceImpl(stockRepository);
+        stock = Stock.builder().idStock(1).title("Title").build();
+        given(stockRepository.save(stock)).willReturn(stock);
+        Stock savedStock = stockService.addStock(stock);
+        assertThat(savedStock).isNotNull();
+    }
+}
